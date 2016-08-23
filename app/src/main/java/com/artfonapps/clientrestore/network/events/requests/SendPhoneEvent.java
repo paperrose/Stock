64c0dccf50d3a675fd790337d9357b0dd3b437df
paperrose <paperrose@mail.ru>
2016-08-23 15:04:05 +0300

    new events

---
+++ app/src/main/java/com/artfonapps/clientrestore/network/events/requests/SendPhoneEvent.java
@@ -0,0 +1,18 @@
+package com.artfonapps.clientrestore.network.events.requests;
+
+import com.artfonapps.clientrestore.network.events.BaseEvent;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+
+/**
+ * Created by Emil on 23.08.2016.
+ */
+public class SendPhoneEvent extends BaseEvent {
+    public SendPhoneEvent(ResponseBody responseBody) throws IOException, JSONException {
+        super(responseBody);
+    }
+}
