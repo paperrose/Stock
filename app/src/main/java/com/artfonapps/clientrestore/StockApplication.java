39eeaf518ed16015e1c3fb178bf58f07acf8a37f
paperrose <paperrose@mail.ru>
2016-08-31 12:36:50 +0300

    logout when enter from another device static prefs and context

---
+++ app/src/main/java/com/artfonapps/clientrestore/StockApplication.java
@@ -0,0 +1,28 @@
+package com.artfonapps.clientrestore;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by Emil on 31.08.2016.
+ */
+public class StockApplication extends com.activeandroid.app.Application {
+    private static final String GCM_PREFS = "GCM_prefs";
+    private static Context appContext;
+    private static SharedPreferences prefs;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        appContext = getApplicationContext();
+        prefs = getSharedPreferences(GCM_PREFS, 0);
+    }
+
+    public static Context getContext() {
+        return appContext;
+    }
+
+    public static SharedPreferences getPrefs() {
+        return prefs;
+    }
+}
